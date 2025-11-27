032980084732adf4a6f0a24c9776a44b003b69c9
headlet <professionalskill6@gmail.com>
2025-11-27 09:59:27 +0545

    made nav bar responsive

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,14 +38,22 @@ return [
             'report' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL') . '/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => base_path('../public_html/storage'),  // points inside public_html
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
         ],
+
 
         's3' => [
             'driver' => 's3',
